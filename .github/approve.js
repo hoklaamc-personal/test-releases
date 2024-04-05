f913954525fe89c1e1587efe8ccba4feb2ee0da6
Hok Laam Cheng <hoklaam.cheng@gmail.com>
2024-04-05 16:03:59 -0400

    separate js file

---
+++ .github/approve.js
@@ -0,0 +1,9 @@
+module.exports = ({github, context}) => {
+  const reviewers = github.rest.pulls.listRequestedReviewers({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.issue.number
+  });
+  console.log(reviewers);
+  console.log(reviewers);
+}
